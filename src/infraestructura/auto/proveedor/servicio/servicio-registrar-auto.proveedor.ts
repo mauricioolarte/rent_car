fc499a8652cdcce949ef35ae1bae0b798d733e42
Rene Mauricio Olarte - Ceiba Software <mauricio.olarte@ceiba.com.co>
2021-12-20 08:07:05 -0500

    maints

--- src/infraestructura/auto/proveedor/servicio/servicio-registrar-auto.proveedor.ts
+++ src/infraestructura/auto/proveedor/servicio/servicio-registrar-auto.proveedor.ts
@@ -1,5 +1,5 @@
 import { RepositorioAuto } from 'src/dominio/auto/puerto/repositorio/repositorio-auto';
-import { ServicioRegistrarAuto } from 'src/dominio/Auto/servicio/servicio-registrar-auto';
+import { ServicioRegistrarAuto } from 'src/dominio/auto/servicio/servicio-registrar-auto';
 
 export function servicioRegistrarAutoProveedor(repositorioAuto: RepositorioAuto) {
   return new ServicioRegistrarAuto(repositorioAuto);
